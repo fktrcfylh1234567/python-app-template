bce1d4bfe64dcff4bec15ac4088ea22340b0db5f
Aleksander Vorobiev <avorobiev@cybertonica.com>
2021-10-19 03:51:14 +0300

    add http_client_examples

---
+++ examples/http_client_examples.py
@@ -0,0 +1,31 @@
+import asyncio
+from dataclasses import dataclass
+
+from dataclasses_json import dataclass_json
+
+from lib.api import request_async
+
+
+@dataclass_json
+@dataclass
+class MyClass:
+    id: int
+    name: str
+
+
+async def main():
+    print("hello async world")
+
+    res = await request_async('get', 'https://httpbin.org/get')
+    print(res)
+
+    my_class = MyClass(1, "zuzuka")
+    res = await request_async('post', 'https://httpbin.org/post', json=my_class)
+    print(res)
+
+    res = await request_async('post', 'https://httpbin.org/post', json=[my_class])
+    print(res)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- main.py
+++ main.py
@@ -1,41 +1,15 @@
 import asyncio
-from dataclasses import dataclass
-
-from dataclasses_json import dataclass_json
-
-from lib.api import request_async
 
 
-@dataclass_json
-@dataclass
-class MyClass:
-    id: int
-    name: str
+async def foo():
+    return 42
 
 
 async def main():
-    print("hello async world")
-
-    res = await request_async('get', 'https://httpbin.org/get')
-    print(res)
-
-    my_class = MyClass(1, "zuzuka")
-    res = await request_async('post', 'https://httpbin.org/post', json=my_class)
-    print(res)
-
-    res = await request_async('post', 'https://httpbin.org/post', json=[my_class])
-    print(res)
-
-
-def deco(fn):
-    print(fn)
-    return fn
-
-
-@deco
-async def foo():
-    pass
+    res = await foo()
+    return res
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    res = asyncio.run(main())
+    print(res)
